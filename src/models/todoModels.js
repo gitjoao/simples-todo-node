6034d1d29070f857240f23ce4a5ae7614d9721a3
jgbatista <joao.moraes.batista@hotmail.com>
2021-02-28 19:50:50 -0300

    novas rotas get put delete

--- src/models/todoModels.js
+++ src/models/todoModels.js
@@ -7,3 +7,19 @@ const todo = [
 exports.getAll = () => { return todo }
 
 exports.save = (newTodo) => { todo.push(newTodo)}
+
+exports.findById = (id) => {return todo.find(e => e.id == id) }
+
+exports.editById = (id, newItem) => { 
+  const todoItem = todo.find(e => e.id == id)
+  todoItem.info = newItem
+  return todoItem
+}
+
+exports.delete = (id) => { 
+  const todoItem = todo.find(e => e.id == id)
+  const index = todo.indexOf(todoItem)
+  if (index > -1) {
+    todo.splice(index, 1)
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 router.get('/', function (req, res, next) {
     res.status(200).send({
         title: "Node Express API",
-        version: "0.0.1"
+        version: "0.0.1",
+        get: "Route to list all itens GET - /todo",
+        post: "Route to create itens POST - /todo body with property 'info' JSON",
+        getIten: "Route to get item GET - /todo/{id}",
+        put: "Route to edit item PUT - /todo/{id} body with property 'info' JSON",
+        delete: "Route to delete item DELETE - /todo/{id}",
     });
 });
 module.exports = router;
